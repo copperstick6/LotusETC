73a0c90819611d137724eacf39daa9e77c5208a1
copperstick6 <han818william@hotmail.com>
2016-10-01 12:09:14 -0500

    CHECKS FOR IF ITS WORTH IT TO BUY XLF
    Checks if it is worth it to buy XLF

---
+++ src/lotus/checkingXLF.java
@@ -0,0 +1,18 @@
+public class checkingXLF
+{
+    public final int XLFfee = 100;
+    public boolean worthItToConvert(Stocks(int,int,int,int,int,int,int)a)
+    {
+      int totVal = 0;
+      if (a.getBONDamt()>=3&&a.getGSamt().=2&&a.getMSamt()>=3%%a.getWFCamt()>=2)
+      {
+        totVal = 3*a.BONDamt.getBuyVal() + 2*a.GSamt.getBuyVal() + 3*a.MSamt.getBuyVal() + 2*WFCamt.getBuyVal + XLFfee;
+        if (totVal<a.XLFamt.getSellVal())
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+  
+}
